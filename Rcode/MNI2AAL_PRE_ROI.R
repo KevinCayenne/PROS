dacd3a07cb78bde2c15367431301a5acd4f05f67
kevin <kevin81319@gmail.com>
2018-10-17 18:40:50 +0800

    add PFC MNI 2 AAL function

---
+++ Rcode/MNI2AAL_PRE_ROI.R
@@ -0,0 +1,7 @@
+setwd("c:/Users/acer/Desktop/PROS/Data/fMRI_PilotData/ROI")
+
+PFC_ROI_coordiates <- read.csv("PFC_ROI_coordiates.csv", header = T)
+
+Result <- t(mapply(FUN=mni_to_region_name,x=PFC_ROI_coordiates$x,y=PFC_ROI_coordiates$y,z=PFC_ROI_coordiates$z))
+
+write.csv(Result, "aal_Result.csv")
